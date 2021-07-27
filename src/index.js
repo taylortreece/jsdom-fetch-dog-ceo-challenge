5366f7bd264aed40f52fcb345eec9c0125d33d8a
taylortreece <tayloratreece@gmail.com>
2021-07-27 13:58:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const dogImageContainer = document.getElementById("dog-image-container")
+
 console.log('%c HI', 'color: firebrick')
+
+window.addEventListener('load', (e) => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(resp => resp.json())
+        .then(json => {
+            for(const element of json.message) {
+                console.log(element)
+            }
+        });
+})
